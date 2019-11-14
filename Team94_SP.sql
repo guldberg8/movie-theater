e36149eee8f773714586660eac98ee1a9bc473ef
carolinercrain <57014173+carolinercrain@users.noreply.github.com>
2019-11-14 14:45:22 -0500

    Update Team94_SP.sql

--- Team94_SP.sql
+++ Team94_SP.sql
@@ -1,3 +1,5 @@
+
+
 DROP PROCEDURE IF EXISTS user_register;
 DELIMITER $$
 CREATE PROCEDURE `user_register`(IN i_username VARCHAR(50), IN i_password VARCHAR(50), IN i_firstname VARCHAR(50), IN i_lastname VARCHAR(50))
@@ -5,6 +7,8 @@ BEGIN
 		INSERT INTO user (username, password, firstname, lastname) VALUES (i_username, MD5(i_password), i_firstname, i_lastname);
 END$$
 DELIMITER ;
+
+
 
 DROP PROCEDURE IF EXISTS user_filter_th;
 DELIMITER $$
@@ -22,6 +26,9 @@ BEGIN
 END$$
 DELIMITER ;
 
+
+
+
 DROP PROCEDURE IF EXISTS user_visit_th;
 DELIMITER $$
 CREATE PROCEDURE `user_visit_th`(IN i_thName VARCHAR(50), IN i_comName VARCHAR(50), IN i_visitDate DATE, IN i_username VARCHAR(50))
@@ -30,6 +37,9 @@ BEGIN
     VALUES (i_thName, i_comName, i_visitDate, i_username);
 END$$
 DELIMITER ;
+
+
+
 
 DROP PROCEDURE IF EXISTS user_filter_visitHistory;
 DELIMITER $$
@@ -47,3 +57,218 @@ BEGIN
         (i_maxVisitDate IS NULL OR visitDate <= i_maxVisitDate);
 END$$
 DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS user_login;
+DELIMITER $$
+CREATE PROCEDURE `user_login`()
+BEGIN
+		
+END$$
+DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS customer_only_register;
+DELIMITER $$
+CREATE PROCEDURE `customer_only_register`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS customer_add_creditcard;
+DELIMITER $$
+CREATE PROCEDURE `customer_add_creditcard`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS manager_only_register;
+DELIMITER $$
+CREATE PROCEDURE `manager_only_register`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS manager_customer_register;
+DELIMITER $$
+CREATE PROCEDURE `manager_customer_register`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS manager_customer_add_creditcard;
+DELIMITER $$
+CREATE PROCEDURE `manager_customer_add_creditcardn`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS admin_filter_user;
+DELIMITER $$
+CREATE PROCEDURE `admin_filter_user`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS admin_approve_user;
+DELIMITER $$
+CREATE PROCEDURE `admin_approve_user`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS admin_decline_user;
+DELIMITER $$
+CREATE PROCEDURE `admin_decline_user`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS admin_filter_company;
+DELIMITER $$
+CREATE PROCEDURE `admin_filter_company`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS admin_create_theater;
+DELIMITER $$
+CREATE PROCEDURE `admin_create_theater`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS admin_view_comDetail_emp;
+DELIMITER $$
+CREATE PROCEDURE `admin_view_comDetail_emp`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS admin_view_comDetail_th;
+DELIMITER $$
+CREATE PROCEDURE `admin_view_comDetail_th`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS admin_create_mov;
+DELIMITER $$
+CREATE PROCEDURE `admin_create_mov`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS manager_filter_th;
+DELIMITER $$
+CREATE PROCEDURE `manager_filter_th`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS manager_schedule_mov;
+DELIMITER $$
+CREATE PROCEDURE `manager_schedule_mov`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+DROP PROCEDURE IF EXISTS customer_filter_mov;
+DELIMITER $$
+CREATE PROCEDURE `customer_filter_mov`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS customer_view_mov;
+DELIMITER $$
+CREATE PROCEDURE `customer_view_mov`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS customer_view_history;
+DELIMITER $$
+CREATE PROCEDURE `customer_view_history`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS user_visit_th;
+DELIMITER $$
+CREATE PROCEDURE `user_visit_th`()
+BEGIN
+        
+END$$
+DELIMITER ;
+
+
+
+
+
+
