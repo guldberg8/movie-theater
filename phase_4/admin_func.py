e8cadbb095bd530e1ceeeaf5fc465f6e90d260b0
carolinercrain <57014173+carolinercrain@users.noreply.github.com>
2019-11-29 21:38:40 -0500

    Update admin_func.py

--- phase_4/admin_func.py
+++ phase_4/admin_func.py
@@ -39,11 +39,31 @@ class manage_user(QDialog):
 
         vbox.addItem(row1)
         self.setLayout(vbox)
+        
+        #username box
+        #status of all, pending, declined, approved
+        #filter 
+        # appprove on Decline buttons
+        # table of username, credit card count, user type, and status
 
 class manage_comp(QDialog):
     def __init__(self):
         super(manage_comp, self).__init__()
         self.setWindowTitle("Manage Company")
+          vbox = QVBoxLayout()
+        row1 = QHBoxLayout()
+        row1.addWidget(QLabel('Username:'))
+        row1.addWidget(self.username)
+        row1.addWidget(QLabel('# City Covered'))
+        row1.addWidget(self.status)
+
+        #name -- dropdown of theater (names individually, and -ALL-)
+        # table of sortable columns 
+        # City Covered
+        # # Theaters
+        # # Employees
+        # table of Name, #CityCovered, #Theaters, #Employee 
+
       
 class create_theater(QDialog):
     def __init__(self):
